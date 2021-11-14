d2af540f1588b6001fdaf36eb7813de48c6a6918
erebz <ebobow@gmail.com>
2021-11-14 23:11:34 +0100

    Exécutable

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,11 +9,11 @@ public class Main {
 
         System.out.println("Début du progamme.");
         // Création du Système multi-agent
-        Scanner myInput = new Scanner(System.in);
-        int nbAgents = 4;
+        Scanner input = new Scanner(System.in);
+        int nbAgents;
         do{
             System.out.print("Veuillez entrer le nombre d'agent à générer : ");
-            nbAgents = myInput.nextInt();
+            nbAgents = input.nextInt();
         }while (nbAgents <= 0);
 
         ArrayList<Agent> agents = new ArrayList<Agent>();
@@ -22,7 +23,6 @@ public class Main {
             Agent a = new Agent(id, but);
             agents.add(a);
         }
-
         ArrayList<ArrayList<Agent>> init = new ArrayList<ArrayList<Agent>>();
         ArrayList<Agent> pile1 = new ArrayList<>();
         ArrayList<Agent> pile2 = new ArrayList<>();
@@ -31,17 +31,32 @@ public class Main {
         for(Agent a : agents){
             pile1.add(a);
         }
-
         init.add(pile1);
         init.add(pile2);
         init.add(pile3);
 
         Environnement monde = new Environnement(init);
         SMA sma = new SMA(agents, monde);
+
+        int choix;
+        do{
+            System.out.println("Veuillez choisir la version à utiliser (1 ou 2) : ");
+            System.out.println("- Version 1 : actions individuelles.");
+            System.out.println("- Version 2 : plannification d'après toutes les perceptions.");
+            choix = input.nextInt();
+        }while (choix != 1 && choix != 2);
+
         long debut = System.nanoTime();
-        sma.runV1();
+        if (choix == 1) sma.runV1();
+        else sma.runV2();
         double fin = (double) (System.nanoTime() - debut)/1000000;
 
         System.out.println("Fin du programme. Temps de résolution : " + String.format("%.3g", fin) + "ms");
+        System.out.print("Appuyez sur Entrée pour terminer.");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
